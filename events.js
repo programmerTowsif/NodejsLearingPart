f04683cd3c2762e499cda540d8c0f45b7620769a
programmerTowsif <programmer.towsif@gmail.com>
2024-05-16 17:41:58 +0600

    update

--- events.js
+++ events.js
@@ -4,55 +4,56 @@ const fs = require('fs')
 const path = require('path')
 // // we buy a class from EventEmitter
 const emitter = new EventEmitter()
-// // after envent 
-// emitter.on('On',(a,b)=>{
-//     let time = 0;
-//       setTimeout(()=>{
-//          if(a.length >b.length){
-//             time =5000
-//             console.log(a.length)
-//          }
-//          time = 3000
-//       },5000)
-//     fs.readFile('./file.js',(err,data)=>{
-//         if(!err){
+// after envent 
+emitter.on('On',(a,b)=>{
+    let time = 0;
+      setTimeout(()=>{
+         if(a.length >b.length){
+            time =5000
+            console.log(a.length)
+         }
+         time = 3000
+      },5000)
+    fs.readFile('./file.js',(err,data)=>{
+        if(!err){
             
-//             // console.log(data.toString())
-//         }
-//     })
-// })
-// //  it's event 
-// emitter.emit('On','saa','bs')
-// emitter.on('clint',()=>{
-// const pathbase =  path.basename("/home/towsif/Desktop/nodejsLearing/index.js");
-// console.log(pathbase)
-// })
+            // console.log(data.toString())
+        }
+    })
+})
+//  it's event 
+emitter.emit('On','saa','bs')
+emitter.on('clint',()=>{
+const pathbase =  path.basename("/home/towsif/Desktop/nodejsLearing/index.js");
+console.log(pathbase)
+})
 
 
-// emitter.emit('clint')
+emitter.emit('clint')
 
-// emitter.on('clock',(i)=>{
-//     console.log('clock2',i)
-//     setTimeout(()=>{
-//         console.log(i)
-//     },4000)
-// })
+emitter.on('clock',(i)=>{
+    console.log('clock2',i)
+    setTimeout(()=>{
+        console.log(i)
+    },4000)
+})
 
-// for(let i =0;i<=100;i++){
-//     console.log("i",i)
+for(let i =0;i<=100;i++){
+    console.log("i",i)
     
-//     emitter.emit('clock',i)
-//     console.log('clock',i)
-// }
-emitter.on('Call',(i)=>{
-    console.log('hello',i)
+    emitter.emit('clock',i)
+    console.log('clock',i)
+}
+emitter.on('Call',({perioud,text})=>{
+    console.log('hello',perioud,text)
 })
 
 setTimeout(()=>{
-     const ff =(i)=>{
-        emitter.emit("Call",i);
-     }
-    for(let i = 0;i<=3;i++){
-        ff(i)
-    }
+      
+        emitter.emit("Call",{
+            perioud :1,
+            text:'college choti'
+        });
+  
+   
 },2000)
